8931b49a3acb3402393f048d21b5da64eb6ef4d6
VitaliyLapshakov <vlapshakov@gmail.com>
2019-01-31 21:20:37 +0700

    new version

--- Range_2/src/ru/academits/lapshakov/RangeTaskOnceMore/Range.java
+++ Range_2/src/ru/academits/lapshakov/RangeTaskOnceMore/Range.java
@@ -52,18 +52,28 @@ public class Range {
     }
 
     public Range[] getDifferenceRangeResult(Range secondInterval) {
-        //if belongs each other:
-        if ((secondInterval.to >= this.from && secondInterval.to <= this.to && secondInterval.from < this.from) || (this.to >= secondInterval.from && this.to <= secondInterval.to && this.from < secondInterval.from)) {
-            return new Range[]{new Range(Math.min(this.from, secondInterval.from), Math.max(this.from, secondInterval.from))};
-        } //if there is any intersection:
-        else if (this.to < secondInterval.from || this.from > secondInterval.to) {
-            return new Range[]{new Range(this.from, this.to)};
-        }//there is intersection:
-        else if ((this.from < secondInterval.from && secondInterval.to < this.to) || (secondInterval.from < this.from && this.to < secondInterval.to)) {
-            return new Range[]{new Range((Math.min(this.from, secondInterval.from)), Math.max(this.from, secondInterval.from)), new Range((Math.min(this.to, secondInterval.to)), Math.max(this.to, secondInterval.to))};
-        }//equal:
-        else {
-            return new Range[0];
+        if (this.from < secondInterval.from) {
+            if (secondInterval.from < this.to) {
+                if (secondInterval.to > this.to) {
+                    return new Range[]{new Range(this.from, secondInterval.from)};
+                } else {
+                    return new Range[]{new Range(this.from, secondInterval.from), new Range(secondInterval.to, this.to)};
+                }
+            } else {
+                return new Range[]{new Range(this.from, this.to)};
+            }
+
+        } else if (this.from >= secondInterval.from) {
+            if (secondInterval.to > this.from) {
+                if (secondInterval.to < this.to) {
+                    return new Range[]{new Range(secondInterval.to, this.to)};
+                } else {
+                    return new Range[0];
+                }
+            } else {
+                return new Range[]{new Range(this.from, this.to)};
+            }
         }
+        return new Range[0];
     }
 }
