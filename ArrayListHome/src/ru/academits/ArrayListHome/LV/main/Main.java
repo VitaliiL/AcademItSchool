14d82c42dd31344874a2f0e66a05e00c7b07d60f
VitaliyLapshakov <vlapshakov@gmail.com>
2019-02-23 14:03:55 +0700

    finished.

--- ArrayListHome/src/ru/academits/ArrayListHome/LV/main/Main.java
+++ ArrayListHome/src/ru/academits/ArrayListHome/LV/main/Main.java
@@ -2,7 +2,10 @@ package ru.academits.ArrayListHome.LV.main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
