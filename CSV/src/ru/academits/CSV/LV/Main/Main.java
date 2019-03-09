9369b9ba2f76218186fbc836bd3551162f16d12d
VitaliyLapshakov <vlapshakov@gmail.com>
2019-03-09 10:41:45 +0700

    ready to check.

--- CSV/src/ru/academits/CSV/LV/Main/Main.java
+++ CSV/src/ru/academits/CSV/LV/Main/Main.java
@@ -5,7 +5,7 @@ import ru.academits.CSV.LV.ParseCSVToHTML.ParseCSVToHTML;
 public class Main {
     public static void main(String[] args) {
         try {
-            ParseCSVToHTML.parseCSV("C:\\Users\\vlaps\\IdeaProjects\\academits\\test.txt", "C:\\Users\\vlaps\\IdeaProjects\\academits\\HTML.html");
+            ParseCSVToHTML.parseCSV("CSV.csv", "HTML.html");
         } catch (IllegalArgumentException e) {
             System.out.println("Need two directories: source is CSV file and destination is HTML file.");
         }
