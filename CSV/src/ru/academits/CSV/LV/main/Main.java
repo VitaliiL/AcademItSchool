e451191d8517de2f87c443e4733f26710044f086
VitaliyLapshakov <vlapshakov@gmail.com>
2019-03-23 11:40:07 +0700

    finished for check.

--- CSV/src/ru/academits/CSV/LV/main/Main.java
+++ CSV/src/ru/academits/CSV/LV/main/Main.java
@@ -7,6 +7,7 @@ public class Main {
         if (args.length != 2) {
             System.out.println("You should input 2 paths in the program arguments: source is CSV file and destination is HTML file.");
         } else {
+
             ParseCSVToHTML.parseCSV(args[0], args[1]);
         }
     }
