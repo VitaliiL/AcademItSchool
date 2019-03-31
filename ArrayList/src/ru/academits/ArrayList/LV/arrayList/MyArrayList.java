5600a61e39b583f58a0d3abda1223d92e868c16a
VitaliyLapshakov <vlapshakov@gmail.com>
2019-03-31 14:15:52 +0700

    ready for check.

--- ArrayList/src/ru/academits/ArrayList/LV/arrayList/MyArrayList.java
+++ ArrayList/src/ru/academits/ArrayList/LV/arrayList/MyArrayList.java
@@ -3,94 +3,171 @@ package ru.academits.ArrayList.LV.arrayList;
 import java.util.*;
 
 public class MyArrayList<E> implements List<E> {
+    private static final int DEFAULT_CAPACITY = 10;
     private Object[] items;
-    private int capacity;
+    private int size;
+    private int modCount;
 
     @Override
     public String toString() {
-        return Arrays.toString(Arrays.copyOf(items, capacity));
+        return Arrays.toString(Arrays.copyOf(items, size));
     }
 
-    public MyArrayList(int capacity) {
-        if (capacity < 0) {
-            throw new IllegalArgumentException("Capacity isn't corrected" + capacity);
+    public MyArrayList(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Capacity isn't corrected" + size);
         }
 
-        items = new Object[capacity];
+        items = new Object[size];
     }
 
     public MyArrayList() {
-        items = new Object[10];
+        items = new Object[DEFAULT_CAPACITY];
     }
 
     @Override
     public int size() {
-        return capacity;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return capacity == 0;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) != -1;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new MyIterator();
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return Arrays.copyOf(items, size);
     }
 
+    @SuppressWarnings("all")
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a.length < size) {
+            return (T[]) Arrays.copyOf(items, size);
+        }
+
+        System.arraycopy(items, 0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
+
     }
 
     @Override
-    public boolean add(E e) {
+    public boolean add(E element) {
+        if (element != null) {
+            items[size] = element;
+            size++;
+
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public boolean remove(Object o) {
+        int index = indexOf(o);
+
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            --size;
+            modCount++;
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        for (Object element : c) {
+            if (!contains(element)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        return addAll(size, c);
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        if (index > size() || index < 0) {
+            throw new IndexOutOfBoundsException("Index isn't corrected.");
+        }
+
+        Object[] array = c.toArray();
+        if (items.length < size + array.length) {
+            items = Arrays.copyOf(items, items.length + array.length);
+        }
+        if (size - index > 0) {
+            System.arraycopy(items, index, items, index + array.length, size - index);
+        }
+
+        System.arraycopy(array, 0, items, index, array.length);
+        size += array.length;
+
+        if (array.length != 0) {
+            modCount++;
+        }
+
+        return array.length != 0;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        boolean modified = false;
+
+        for (int i = 0; i < size; ++i) {
+            if (c.contains(items[i])) {
+                remove(i);
+                --i;
+                modified = true;
+            }
+        }
+
+        return modified;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        boolean modified = false;
+
+        for (int i = 0; i < size; ++i) {
+            if (!c.contains(items[i])) {
+                remove(i);
+                --i;
+                modified = true;
+            }
+        }
+
+        return modified;
     }
 
     @Override
     public void clear() {
+        for (int i = 0; i < size; i++) {
+            items[i] = null;
+        }
 
+        size = 0;
     }
 
     @SuppressWarnings("unchecked")
@@ -129,16 +206,16 @@ public class MyArrayList<E> implements List<E> {
 
     @Override
     public void add(int index, E element) {
-        if (index > capacity || index < 0) {
+        if (index > size || index < 0) {
             throw new IndexOutOfBoundsException("Index isn't corrected.");
         }
 
-        if (items.length <= capacity) {
+        if (items.length <= size) {
             increaseCapacity();
         }
 
         items[index] = element;
-        capacity++;
+        size++;
     }
 
     @Override
@@ -149,36 +226,88 @@ public class MyArrayList<E> implements List<E> {
             throw new IndexOutOfBoundsException("Index isn't corrected.");
         }
 
-        if (index < capacity - 1) {
-            System.arraycopy(items, index + 1, items, index, capacity - index - 1);
+        if (index < size - 1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
         }
-        --capacity;
+        --size;
 
         return item;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(items[i], o)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (Objects.equals(items[i], o)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
+    public void ensureCapacity(int userSize) {
+        if (userSize > items.length) {
+            items = Arrays.copyOf(items, userSize);
+        }
+    }
+
+    public void trimToSize() {
+        if (items.length > size) {
+            items = Arrays.copyOf(items, size);
+        }
+    }
+
+    private class MyIterator implements Iterator<E> {
+        private int currentIndex = -1;
+        private int mod = modCount;
+
+        public boolean hasNext() {
+            return currentIndex + 1 < size;
+        }
+
+        public E next() {
+            if (currentIndex == size) {
+                throw new NoSuchElementException("Collection is finished.");
+            }
+
+            if (mod != modCount) {
+                throw new ConcurrentModificationException("Collection is changed.");
+            }
+
+            ++currentIndex;
+
+            return getItem(currentIndex);
+        }
+    }
+
+
+    //no need:
+    @SuppressWarnings("all")
     @Override
     public ListIterator<E> listIterator() {
         return null;
     }
 
+    @SuppressWarnings("all")
     @Override
     public ListIterator<E> listIterator(int index) {
         return null;
     }
 
+    @SuppressWarnings("all")
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
         return null;
     }
+
 }
--- ArrayList/src/ru/academits/ArrayList/LV/main/Main.java
+++ ArrayList/src/ru/academits/ArrayList/LV/main/Main.java
@@ -2,12 +2,15 @@ package ru.academits.ArrayList.LV.main;
 
 import ru.academits.ArrayList.LV.arrayList.MyArrayList;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //check constructors:
         MyArrayList<Integer> list1 = new MyArrayList<>();
         MyArrayList<Integer> list2 = new MyArrayList<>(3);
 
+        //check methods:
         System.out.println("Check increase capacity list2: " + list2.getCapacity());
         System.out.println("<After filling>:");
 
@@ -16,16 +19,46 @@ public class Main {
             list2.add(i, j);
         }
 
+        list1.add(11, 3);
+        list1.add(5);
+
         System.out.println("Check size list1: " + list1.size());
         System.out.println("Check array if it is empty list1: " + list1.isEmpty());
 
-        System.out.println("Check add by index list" + list1);
+        System.out.println("Check add by index list: " + list1);
+        System.out.println("Check add: " + list1.add(7) + " -> " + list1);
         System.out.println("Check increase capacity list1: " + list1.getCapacity());
-        System.out.println("Check increase capacity list2: " + list2.getCapacity());
+        System.out.println("Check increase capacity list2: " + list2.getCapacity() + "\n");
 
-        System.out.println();
+        System.out.println("Check remove element by index: " + list1.remove(3) + " -> " + list1);
+        System.out.println("Check remove by element: " + list1.remove(Integer.valueOf(5)) + " -> " + list1);
 
-        System.out.println("Check remove element by index: " + list1.remove(3));
-        System.out.println("Check remove from list1: " + list1);
+        list1.trimToSize();
+        System.out.println("Check capacity after trimToSize: " + list1.getCapacity());
+        list1.ensureCapacity(20);
+        System.out.println("Check capacity after ensureCapacity: " + list1.getCapacity() + "\n");
+        System.out.println("Check contains: " + list1.contains(3));
+
+        Object[] array = list1.toArray();
+        System.out.println("Check to Array methods: " + Arrays.toString(array));
+
+        System.out.println("Check the first occurrence: " + list1.indexOf(3));
+        System.out.println("Check the latest occurrence: " + list1.lastIndexOf(3) + "\n");
+
+        MyArrayList<Integer> listAddition = new MyArrayList<>();
+        listAddition.add(22);
+        listAddition.add(33);
+
+        System.out.println("Check addAll by index: " + list1.addAll(1, listAddition) + " -> " + list1);
+        System.out.println("Check addAll: " + list1.addAll(list2) + " -> " + list1 + "\n");
+        System.out.println("Check containsAll: " + list1.containsAll(listAddition));
+
+        System.out.println(list1);
+        System.out.println(list2);
+        System.out.println("Check retainAll: " + list2.retainAll(list1) + "->" + list2);
+        System.out.println("Check removeAll: " + list2.removeAll(list1) + "->" + list2 + "\n");
+
+        list1.clear();
+        System.out.println("Clear list1: " + list1);
     }
 }
